b1ab1ac97169351377ec33b61757ee5966593161
drakotech <george.flefel@gmail.com>
2021-07-11 13:58:26 -0700

    Deleting branches

---
+++ show-notes/git-delete-branch.py
@@ -0,0 +1,10 @@
+""" 
+How to Delete a Local and Remote Branch in Git
+
+
+Best practice is to delete your branches once you are done with
+them to avoid confusion, especially if there is a lot of time
+between working on a branch. It can be difficult to sort through
+the clutter.
+
+"""
