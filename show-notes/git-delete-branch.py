0815013eda87a8038f47d2efa77d24c659f6acac
drakotech <george.flefel@gmail.com>
2021-07-11 14:06:16 -0700

    Deleting branches from remote and local repositories

--- show-notes/git-delete-branch.py
+++ show-notes/git-delete-branch.py
@@ -7,4 +7,24 @@ them to avoid confusion, especially if there is a lot of time
 between working on a branch. It can be difficult to sort through
 the clutter.
 
+
+git branch -d branch-to-remove
+
+#warning: deleting branch 'branch-to-remove' that has been 
+# merged to
+#         'refs/remotes/origin/branch-to-remove', but not yet
+# merged to HEAD.
+#Deleted branch branch-to-remove (was 817d3a2).
+
+  This deleted the branch from the local repository and
+  hasn't been removed from the remote repository yet.
+
+
+git push origin --delete branch-to-remove
+
+#To https://github.com/drakotech/git.git
+# - [deleted]         branch-to-remove
+
+  This will remove the remote branch from github.com
+
 """
