92a4f66c9ba5d611e3097ebd4806430c5b18d668
drakotech <george.flefel@gmail.com>
2021-07-15 14:01:34 -0700

    Reverted back to 400e3135d32f8b19c2b315f420a5346cdd0f7996 for my_file.py

--- my_file.py
+++ my_file.py
@@ -4,5 +4,3 @@ print('Hi from another dev')
 
 def hi_there():
     print('hi')
-def hi_there():
-    print('Update python function with merge conflict')
---
+++ show-notes/git-revert.py
@@ -0,0 +1,16 @@
+"""
+Reverting to the Latest Commit in Git
+
+
+To revert back to a previous commit session you can run:
+
+git checkout .
+
+#Updated 2 paths from the index  
+
+  This will undo all changes made since last commit. It will
+  not effect any untracked files but it will revert the changes
+  made to tracked files into how they were before the commit.
+
+
+"""
