596b0073b3e4975f62b5531577f871ef4cc38abf
drakotech <george.flefel@gmail.com>
2021-07-15 15:01:51 -0700

    Revert file

---
+++ show-notes/git-revert-file.py
@@ -0,0 +1,17 @@
+"""
+How to Revert a Single File to a Previous Version in Git
+
+
+git checkout 400e3135d32f8b19c2b315f420a5346cdd0f7996 -- my_file.py
+
+When you want to revert only a file back to its previous version
+you can run the above command from the terminal. 
+
+To get the commit id you can run:
+
+git log
+
+and find the commit by pressing "j" to go down or "k" to go up.
+To go all the way to the end tap "shift g".
+
+"""
